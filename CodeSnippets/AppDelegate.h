a2655633b1f701349d8df6a1f5b309f2b2eda828
Yangzhe <669672615@qq.com>
2016-01-08 09:52:07 +0800

    Initial Commit

---
+++ CodeSnippets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodeSnippets
+//
+//  Created by suorui on 1/8/16.
+//  Copyright © 2016 GeekRRK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodeSnippets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeSnippets
+//
+//  Created by suorui on 1/8/16.
+//  Copyright © 2016 GeekRRK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
