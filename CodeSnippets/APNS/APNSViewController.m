dc26da56b3398eff46ca43fa72c5bb482e975ce0
Yangzhe <669672615@qq.com>
2016-01-11 17:02:25 +0800

    Update APNS

--- CodeSnippets/APNS/APNSViewController.m
+++ CodeSnippets/APNS/APNSViewController.m
@@ -75,4 +75,77 @@
     }
 }
 
+static void completionCallback(SystemSoundID  mySSID) {
+    AudioServicesPlaySystemSound(mySSID);
+}
+
+- (void)playSound {
+    // Get the main bundle for the app
+    CFBundleRef mainBundle;
+    SystemSoundID soundFileObject;
+    mainBundle = CFBundleGetMainBundle();
+    
+    // Get the URL to the sound file to play
+    CFURLRef soundFileURLRef  = CFBundleCopyResourceURL (
+                                                         mainBundle,
+                                                         CFSTR ("background" ),
+                                                         CFSTR ("wav" ),
+                                                         NULL
+                                                         );
+    // Create a system sound object representing the sound file
+    AudioServicesCreateSystemSoundID (
+                                      soundFileURLRef,
+                                      &soundFileObject
+                                      );
+    // Add sound completion callback
+    AudioServicesAddSystemSoundCompletion (soundFileObject, NULL , NULL ,
+                                        (void *) completionCallback,
+                                           (__bridge void *) self );
+    // Play the audio
+    AudioServicesPlaySystemSound(soundFileObject);
+    
+}
+
+// Define a callback to be called when the sound is finished
+// playing. Useful when you need to free memory after playing.
+static void MyCompletionCallback (
+                                  SystemSoundID  mySSID,
+                                  void * myURLRef
+                                  ) {
+    AudioServicesDisposeSystemSoundID (mySSID);
+    CFRelease (myURLRef);
+    
+    CFRunLoopStop (CFRunLoopGetCurrent());
+}
+
+- (void)playSound2 {
+    // Set up the pieces needed to play a sound.
+    SystemSoundID    mySSID;
+    CFURLRef        myURLRef;
+    myURLRef = CFURLCreateWithFileSystemPath (
+                                              kCFAllocatorDefault,
+                                              CFSTR ("http://www.cnblogs.com/ComedyHorns.aif"),
+                                              kCFURLPOSIXPathStyle,
+                                              FALSE
+                                              );
+    // create a system sound ID to represent the sound file
+    OSStatus error = AudioServicesCreateSystemSoundID (myURLRef,
+                                                       &mySSID);
+    // Register the sound completion callback.
+    // Again, useful when you need to free memory after playing.
+    AudioServicesAddSystemSoundCompletion (
+                                           mySSID,
+                                           NULL,
+                                           NULL,
+                                           MyCompletionCallback,
+                                           (void *) myURLRef
+                                           );
+    // Play the sound file.
+    AudioServicesPlaySystemSound (mySSID);
+    // Invoke a run loop on the current thread to keep the application
+    // running long enough for the sound to play; the sound completion
+    // callback later stops this run loop.
+    CFRunLoopRun ();
+}
+
 @end
