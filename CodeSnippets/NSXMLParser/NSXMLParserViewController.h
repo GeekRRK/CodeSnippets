5a45f0f635edc3495ba4b1bfee6b624ead31bb97
Yangzhe <669672615@qq.com>
2016-01-13 12:33:13 +0800

    Update

--- CodeSnippets/NSXMLParser/NSXMLParserViewController.h
+++ CodeSnippets/NSXMLParser/NSXMLParserViewController.h
@@ -2,7 +2,7 @@
 //  NSXMLParserViewController.h
 //  CodeSnippets
 //
-//  Created by suorui on 1/12/16.
+//  Created by GeekRRK on 1/12/16.
 //  Copyright © 2016 GeekRRK. All rights reserved.
 //
 
