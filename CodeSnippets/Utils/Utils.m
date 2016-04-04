9b5bd5db033f9161ad8bae46235eed83c374d319
GeekRRK <669672615@qq.com>
2016-04-04 13:21:19 +0800

    Update Utils.m

--- CodeSnippets/Utils/Utils.m
+++ CodeSnippets/Utils/Utils.m
@@ -1,16 +1,3 @@
-#define WEAKSELF typeof(self) __weak weakSelf = self;
-#define APPDELEGATE ((AppDelegate *)[[UIApplication sharedApplication] delegate])
-
-#define cSCREENWIDTH [[UIScreen mainScreen] bounds].size.width
-#define cSCREENHEIGHT [[UIScreen mainScreen] bounds].size.height
-
-//#define __GEEKDEBUG__
-#ifdef __GEEKDEBUG__
-    #define SERVERADDR @""
-#else
-    #define SERVERADDR @""
-#endif
-
 + (void)showMessage:(NSString *)message
 {
     UIWindow *window = [UIApplication sharedApplication].keyWindow;
