55f5804ae7fedc450f5463a07d722f2ed95c75cf
GeekRRK <669672615@qq.com>
2016-04-07 15:58:09 +0800

    Update Utils

---
+++ CodeSnippets/Utils/Utils.h
@@ -0,0 +1,13 @@
+//
+//  Utils.h
+//  CodeSnippets
+//
+//  Created by UGOMEDIA on 16/4/7.
+//  Copyright © 2016年 GeekRRK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utils : NSObject
+
+@end
--- CodeSnippets/Utils/Utils.m
+++ CodeSnippets/Utils/Utils.m
@@ -1,3 +1,18 @@
+//
+//  Utils.m
+//  CodeSnippets
+//
+//  Created by UGOMEDIA on 16/4/7.
+//  Copyright © 2016年 GeekRRK. All rights reserved.
+//
+
+#import "Utils.h"
+#import <UIKit/UIKit.h>
+#import "Common.h"
+#import <CommonCrypto/CommonCrypto.h>
+
+@implementation Utils
+
 + (void)showMessage:(NSString *)message
 {
     UIWindow *window = [UIApplication sharedApplication].keyWindow;
@@ -33,7 +48,7 @@
 }
 
 + (NSMutableArray *) readFileBy:(NSString *)fileName{
-    NSString *filepath = [SRUtil getFilePathBy:fileName];
+    NSString *filepath = [Utils getFilePathBy:fileName];
     NSMutableArray *apps = [[NSMutableArray alloc] initWithContentsOfFile:filepath];
     if(apps == nil){
         apps = [[NSMutableArray alloc] init];
@@ -46,7 +61,7 @@
 }
 
 + (NSMutableDictionary *)readDictBy:(NSString *)fileName{
-    NSString *filepath = [SRUtil getFilePathBy:fileName];
+    NSString *filepath = [Utils getFilePathBy:fileName];
     NSMutableDictionary *apps = [[NSMutableDictionary alloc] initWithContentsOfFile:filepath];
     if(apps == nil){
         apps = [[NSMutableDictionary alloc] init];
@@ -93,23 +108,23 @@
 }
 
 + (void) writeArray:(NSMutableArray *)array to:(NSString *)fileName{
-    NSString *path = [SRUtil getFilePathBy:fileName];
+    NSString *path = [Utils getFilePathBy:fileName];
     [array writeToFile:path atomically:YES];
 }
 
 + (void) writeDict:(NSMutableDictionary *)dict to:(NSString *)fileName{
-    NSString *path = [SRUtil getFilePathBy:fileName];
+    NSString *path = [Utils getFilePathBy:fileName];
     [dict writeToFile:path atomically:YES];
 }
 
 + (NSData *)readDataBy:(NSString *)fileName{
-    NSString *filepath = [SRUtil getFilePathBy:fileName];
+    NSString *filepath = [Utils getFilePathBy:fileName];
     NSData *data = [[NSData alloc] initWithContentsOfFile:filepath];
     return data;
 }
 
 + (void)writeData:(NSData *)data to:(NSString *)fileName{
-    NSString *filepath = [SRUtil getFilePathBy:fileName];
+    NSString *filepath = [Utils getFilePathBy:fileName];
     [data writeToFile:filepath atomically:YES];
 }
 
@@ -241,17 +256,17 @@
 }
 
 + (NSString *)reduceImg:(UIImage *)img{
-    UIImage *scaledImg = [SRUtil image:img byScalingToSize:CGSizeMake(150, 150)];
+    UIImage *scaledImg = [Utils image:img byScalingToSize:CGSizeMake(150, 150)];
     
-    NSString *base64Str = [SRUtil base64Img:scaledImg];
+    NSString *base64Str = [Utils base64Img:scaledImg];
     
     return base64Str;
 }
 
 + (NSString *)reduceImg:(UIImage *)img scale:(CGSize)size{
-    UIImage *scaledImg = [SRUtil image:img byScalingToSize:size];
+    UIImage *scaledImg = [Utils image:img byScalingToSize:size];
     
-    NSString *base64Str = [SRUtil base64Img:scaledImg];
+    NSString *base64Str = [Utils base64Img:scaledImg];
     
     return base64Str;
 }
@@ -279,7 +294,7 @@
     {
         return image;
     }
-    CGSize size = [SRUtil fitsize:image.size];
+    CGSize size = [Utils fitsize:image.size];
     UIGraphicsBeginImageContext(size);
     CGRect rect = CGRectMake(0, 0, size.width, size.height);
     [image drawInRect:rect];
@@ -313,3 +328,118 @@
             ];
     
 }
+
++ (UIImage *) imageCompressForSize:(UIImage *)sourceImage targetSize:(CGSize)size{
+    
+    UIImage *newImage = nil;
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = size.width;
+    CGFloat targetHeight = size.height;
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0, 0.0);
+    
+    if(CGSizeEqualToSize(imageSize, size) == NO){
+        
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if(widthFactor > heightFactor){
+            scaleFactor = widthFactor;
+            
+        }
+        else{
+            
+            scaleFactor = heightFactor;
+        }
+        scaledWidth = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        
+        if(widthFactor > heightFactor){
+            
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+        }else if(widthFactor < heightFactor){
+            
+            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+        }
+    }
+    
+    UIGraphicsBeginImageContext(size);
+    
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [sourceImage drawInRect:thumbnailRect];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    if(newImage == nil){
+        NSLog(@"scale image fail");
+    }
+    
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
++ (UIImage *) imageCompressForWidth:(UIImage *)sourceImage targetWidth:(CGFloat)defineWidth{
+    
+    UIImage *newImage = nil;
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = defineWidth;
+    CGFloat targetHeight = height / (width / targetWidth);
+    CGSize size = CGSizeMake(targetWidth, targetHeight);
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0, 0.0);
+    
+    if(CGSizeEqualToSize(imageSize, size) == NO){
+        
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if(widthFactor > heightFactor){
+            scaleFactor = widthFactor;
+        }
+        else{
+            scaleFactor = heightFactor;
+        }
+        scaledWidth = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        
+        if(widthFactor > heightFactor){
+            
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+            
+        }else if(widthFactor < heightFactor){
+            
+            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+        }
+    }
+    
+    UIGraphicsBeginImageContext(size);
+    
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [sourceImage drawInRect:thumbnailRect];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    if(newImage == nil){
+        
+        NSLog(@"scale image fail");
+    }
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+@end
