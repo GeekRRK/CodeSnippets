7a9c0cab6d3b4283c5d8e003db4151714c6fd3f0
GeekRRK <669672615@qq.com>
2016-05-11 19:53:46 +0800

    Update SwiftViewController

--- CodeSnippets/Swift/SwiftViewController.swift
+++ CodeSnippets/Swift/SwiftViewController.swift
@@ -8,6 +8,26 @@
 
 import UIKit
 
+enum Rank: Int {
+    case Ace = 1
+    case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
+    func simpleDescription() -> String {
+        switch self {
+        case .Ace:
+            return "ace"
+        case .Jack:
+            return "jack"
+        case .Queen:
+            return "queen"
+        case .King:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+
 class SwiftViewController: UIViewController {
 
     static let sharedInstance = SwiftViewController()
@@ -28,6 +48,11 @@ class SwiftViewController: UIViewController {
         print(total)
         
         print(greet("GeekRRK", day: "Sunday"))
+        
+        if let convertedRank = Rank(rawValue: 11) {
+            let threeDescription = convertedRank.simpleDescription()
+            print(threeDescription)
+        }
     }
     
     func greet(name: String, day: String) -> String {
