b1d32f80f3522cccfb439c090f39a393a05f256e
GeekRRK <669672615@qq.com>
2016-05-11 19:41:45 +0800

    Add Swift

---
+++ CodeSnippets/Swift/SwiftViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftViewController.swift
+//  CodeSnippets
+//
+//  Created by UGOMEDIA on 16/5/11.
+//  Copyright © 2016年 GeekRRK. All rights reserved.
+//
+
+import UIKit
+
+class SwiftViewController: UIViewController {
+
+    static let sharedInstance = SwiftViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
