727835b9133fb35670b02c435b7b4d4a672005ee
GeekRRK <669672615@qq.com>
2016-05-13 09:02:22 +0800

    Replace company name

--- CodeSnippets/KVO/KVOViewController.m
+++ CodeSnippets/KVO/KVOViewController.m
@@ -2,7 +2,7 @@
 //  KVOViewController.m
 //  CodeSnippets
 //
-//  Created by UGOMEDIA on 16/5/4.
+//  Created by GeekRRK on 16/5/4.
 //  Copyright © 2016年 GeekRRK. All rights reserved.
 //
 
--- CodeSnippets/Swift/SwiftViewController.swift
+++ CodeSnippets/Swift/SwiftViewController.swift
@@ -2,7 +2,7 @@
 //  SwiftViewController.swift
 //  CodeSnippets
 //
-//  Created by UGOMEDIA on 16/5/11.
+//  Created by GeekRRK on 16/5/11.
 //  Copyright © 2016年 GeekRRK. All rights reserved.
 //
 
