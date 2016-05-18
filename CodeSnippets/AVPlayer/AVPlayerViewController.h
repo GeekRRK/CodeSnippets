1755309d73d1977b7e4a9e13d15d8eb10361b883
GeekRRK <669672615@qq.com>
2016-05-18 15:51:06 +0800

    Add AVPlayerViewController

---
+++ CodeSnippets/AVPlayer/AVPlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  AVPlayerViewController.h
+//  CodeSnippets
+//
+//  Created by GeekRRK on 16/5/18.
+//  Copyright © 2016年 GeekRRK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AVPlayerViewController : UIViewController
+
+@end
---
+++ CodeSnippets/AVPlayer/AVPlayerViewController.m
@@ -0,0 +1,22 @@
+//
+//  AVPlayerViewController.m
+//  CodeSnippets
+//
+//  Created by GeekRRK on 16/5/18.
+//  Copyright © 2016年 GeekRRK. All rights reserved.
+//
+
+#import "AVPlayerViewController.h"
+
+@interface AVPlayerViewController ()
+
+@end
+
+@implementation AVPlayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
