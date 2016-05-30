432dea1670934bd2bb3d3a38eefeb5c5d4264457
GeekRRK <669672615@qq.com>
2016-05-30 11:44:22 +0800

    Add Category

---
+++ CodeSnippets/Category/UIImage+RoundedCorner.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+RoundedCorner.h
+//  CodeSnippets
+//
+//  Created by UGOMEDIA on 16/5/30.
+//  Copyright © 2016年 GeekRRK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (RoundedCorner)
+
+- (UIImage *)roundedCornerImageWithCornerRadius:(CGFloat)cornerRadius;
+
+@end
---
+++ CodeSnippets/Category/UIImage+RoundedCorner.m
@@ -0,0 +1,34 @@
+//
+//  UIImage+RoundedCorner.m
+//  CodeSnippets
+//
+//  Created by UGOMEDIA on 16/5/30.
+//  Copyright © 2016年 GeekRRK. All rights reserved.
+//
+
+#import "UIImage+RoundedCorner.h"
+
+@implementation UIImage (RoundedCorner)
+
+- (UIImage *)roundedCornerImageWithCornerRadius:(CGFloat)cornerRadius {
+    CGFloat w = self.size.width;
+    CGFloat h = self.size.height;
+    CGFloat scale = [UIScreen mainScreen].scale;
+    
+    if (cornerRadius < 0)
+        cornerRadius = 0;
+    else if (cornerRadius > MIN(w, h))
+        cornerRadius = MIN(w, h) / 2.;
+    
+    UIImage *image = nil;
+    CGRect imageFrame = CGRectMake(0., 0., w, h);
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, scale);
+    [[UIBezierPath bezierPathWithRoundedRect:imageFrame cornerRadius:cornerRadius] addClip];
+    [self drawInRect:imageFrame];
+    image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
+
+@end
