702b8277964c9b06820c87114770ecaa1381b509
GeekRRK <669672615@qq.com>
2016-07-19 14:33:35 +0800

    Add UIImage+scale

---
+++ CodeSnippets/Category/UIImage+scale.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+scale.h
+//  CodeSnippets
+//
+//  Created by UGOMEDIA on 16/7/19.
+//  Copyright © 2016年 GeekRRK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (scale)
+
+@end
