d6a792f1afe6bd7ec6ef33f3a4b2afc4879a1f4a
GeekRRK <669672615@qq.com>
2016-07-29 06:26:29 +0800

    Create UITextFieldVC.m

---
+++ CodeSnippets/UITextField/UITextFieldVC.m
@@ -0,0 +1,10 @@
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    // Prevent crashing undo bug – see note below.
+    if(range.length + range.location > textField.text.length)
+    {
+        return NO;
+    }
+    
+    NSUInteger newLength = [textField.text length] + [string length] - range.length;
+    return newLength <= 11;
+}
