f8ddcc8e1de21e017aab5b891e33e0a6ffbfabd9
GeekRRK <669672615@qq.com>
2016-08-08 14:46:42 +0800

    Add the origin website to AOP

--- CodeSnippets/AOP/HookUtility.h
+++ CodeSnippets/AOP/HookUtility.h
@@ -6,6 +6,8 @@
 //  Copyright © 2016年 GeekRRK. All rights reserved.
 //
 
+// Refer to: http://www.cocoachina.com/ios/20160421/15912.html
+
 #import <Foundation/Foundation.h>
 
 @interface HookUtility : NSObject
