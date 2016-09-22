3886f48d23115358a22c44670b9aac62ab1ebc6b
GeekRRK <669672615@qq.com>
2016-09-22 11:47:29 +0800

    Add UITableView

---
+++ CodeSnippets/UITableView/UITableViewVC.h
@@ -0,0 +1,13 @@
+//
+//  UITableViewVC.h
+//  CodeSnippets
+//
+//  Created by UGOMEDIA on 16/9/22.
+//  Copyright © 2016年 GeekRRK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableViewVC : UIViewController
+
+@end
---
+++ CodeSnippets/UITableView/UITableViewVC.m
@@ -0,0 +1,71 @@
+//
+//  UITableViewVC.m
+//  CodeSnippets
+//
+//  Created by UGOMEDIA on 16/9/22.
+//  Copyright © 2016年 GeekRRK. All rights reserved.
+//
+
+#import "UITableViewVC.h"
+
+@interface UITableViewVC () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) NSMutableArray *dataArr;
+@property (strong, nonatomic) UITableView *tableView;
+
+@end
+
+@implementation UITableViewVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 0;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    return nil;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *reusableCell = @"ReusableCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reusableCell];
+    if (cell == nil) {
+        cell = [[[NSBundle mainBundle] loadNibNamed:@"XXX" owner:nil options:nil] firstObject];
+    }
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [_dataArr removeObjectAtIndex:indexPath.row];
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+    }
+}
+
+@end
