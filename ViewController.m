7f4782dcaa44d6dd0617ff7380d774ee5b12e859
GeekRRK <669672615@qq.com>
2016-10-14 09:22:39 +0800

    Update ViewController to try NSObject+AlBuilder

--- ViewController.m
+++ ViewController.m
@@ -11,6 +11,7 @@
 #import "AlIndicator.h"
 #import "Utils.h"
 #import "MasonryVC.h"
+#import "NSObject+AlBuilder.h"
 
 @interface ViewController ()
 
@@ -21,8 +22,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    MasonryVC *masonryVC = [[MasonryVC alloc] init];
-    [self.navigationController pushViewController:masonryVC animated:YES];
+    [self setupUI];
+}
+
+- (void)setupUI {
+    // view 1
+    UIView *v1 = [UIView z0_builder:^(UIView *that) {
+        that.frame = CGRectMake(0, 0, 320, 200);
+        that.backgroundColor = [UIColor blackColor];
+    }];
+    
+    // view 2
+    UIView *v2 = [[UIView alloc] init];
+    [v2 z0_builder:^(UIView *that) {
+        that.frame = CGRectMake(0, 0, 320, 200);
+        that.backgroundColor = [UIColor redColor];
+    }];
+    
+    [self.view addSubview:v1];
+    [self.view addSubview:v2];
 }
 
 @end
