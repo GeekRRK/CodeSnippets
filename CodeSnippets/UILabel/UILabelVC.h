433e582cac92bf2cefe86727c671cbcb43c5a970
GeekRRK <669672615@qq.com>
2016-10-18 11:23:45 +0800

    Add UILabelVC

---
+++ CodeSnippets/UILabel/UILabelVC.h
@@ -0,0 +1,13 @@
+//
+//  UILabelVC.h
+//  CodeSnippets
+//
+//  Created by UGOMEDIA on 16/10/18.
+//  Copyright © 2016年 GeekRRK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabelVC : UIViewController
+
+@end
---
+++ CodeSnippets/UILabel/UILabelVC.m
@@ -0,0 +1,28 @@
+//
+//  UILabelVC.m
+//  CodeSnippets
+//
+//  Created by UGOMEDIA on 16/10/18.
+//  Copyright © 2016年 GeekRRK. All rights reserved.
+//
+
+#import "UILabelVC.h"
+
+@interface UILabelVC ()
+
+@end
+
+@implementation UILabelVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UILabel *contentLabel = [[UILabel alloc] init];
+    NSMutableParagraphStyle *paragraph = [[NSMutableParagraphStyle alloc] init];
+    paragraph.lineSpacing = 3;
+    NSMutableAttributedString *attrStr = [[NSMutableAttributedString alloc] initWithString:contentLabel.text];
+    [attrStr addAttribute:NSParagraphStyleAttributeName value:paragraph range:NSMakeRange(0, attrStr.length)];
+    contentLabel.attributedText = attrStr;
+}
+
+@end
